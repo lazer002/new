39dc10d46124b4abb20a98e5cec254eda6008e57
lazer <ajlazer002@gmail.com>
2026-07-18 12:05:17 +0530

    retwo

--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const BASE_URL = "https://urbanzoro.onrender.com/api";
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL
 
 export const api = axios.create({
   baseURL: BASE_URL,
